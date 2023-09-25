be82f5cf7771b98ed5e056374558368cceb3e5e8
Kamila Peres <kmlrp@motorola.com>
2023-09-25 14:42:23 -0400

    Primeiro comit - transversal

---
+++ src/pages/PrivacidadeDeDados/index.tsx
@@ -0,0 +1,35 @@
+import { Fragment } from "react";
+
+export const PrivacidadeDeDados = () => {
+  return (
+    <Fragment>
+      <div>
+        A privacidade de dados diz respeito à proteção dos dados pessoais e
+        sensíveis dos indivíduos contra uso indevido ou divulgação não
+        autorizada. Este tópico é essencial na era da IA, onde a coleta e
+        análise de dados são onipresentes. A questão central aqui é como
+        garantir que as empresas e organizações coletem e utilizem os dados das
+        pessoas de maneira ética, transparente e de acordo com suas
+        preferências. Exemplo: O escândalo do Facebook-Cambridge Analytica em
+        2018 expôs como dados pessoais foram indevidamente utilizados para
+        influenciar eleições e propagar mensagens políticas.
+      </div>
+      <div>
+        Causas: 1. Falta de Consentimento Informado: Muitas vezes, os usuários
+        não são adequadamente informados sobre como seus dados serão usados. 2.
+        Modelos de Negócios Predatórios: Empresas podem coletar dados excessivos
+        para fins de lucro sem considerar a privacidade. 3. Falta de
+        Regulamentação Adequada: A ausência de regulamentação rigorosa permite
+        abusos de dados.
+      </div>
+      <div>
+        Como evitar: 1. Consentimento Transparente: Empresas devem obter
+        consentimento informado dos usuários para coletar e usar dados. 2.
+        Regulamentação Forte: Estabelecer regulamentações robustas que protejam
+        os direitos de privacidade dos cidadãos. 3. Minimização de Dados:
+        Coletar apenas os dados estritamente necessários e armazená-los de forma
+        segura.
+      </div>
+    </Fragment>
+  );
+};
---
+++ src/routes/index.tsx
@@ -0,0 +1,30 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Conclusao } from "../pages/Conclusao";
+import { ImpactoNoMercado } from "../pages/ImpactoNoMercado";
+import { Introducao } from "../pages/Introducao";
+import { PrivacidadeDeDados } from "../pages/PrivacidadeDeDados";
+import { RacismoAlgoritmico } from "../pages/RacismoAlgoritmico";
+import { RoutesEnum } from "./enum";
+
+export default function MainRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={RoutesEnum.Home} element={<Introducao />} />
+        <Route
+          path={RoutesEnum.racismoAlgoritmico}
+          element={<RacismoAlgoritmico />}
+        />
+        <Route
+          path={RoutesEnum.privacidadeDeDados}
+          element={<PrivacidadeDeDados />}
+        />
+        <Route
+          path={RoutesEnum.impactoNoMercado}
+          element={<ImpactoNoMercado />}
+        />
+        <Route path={RoutesEnum.conclusao} element={<Conclusao />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
