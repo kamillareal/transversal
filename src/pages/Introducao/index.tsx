126657f0ddcb974ba09d55017ee757076fe151fd
Arthur Barata <arthurbs@motorola.com>
2023-09-25 17:02:08 -0400

    Co-authored-by: Kamilla Real Peres <kamillareal@users.noreply.github.com>

--- src/pages/Introducao/index.tsx
+++ src/pages/Introducao/index.tsx
@@ -1,13 +1,21 @@
+import brainAi from "";
+import { Fragment } from "react";
+import AppBarHeader from "../../shared/components/app-bar";
+import { Image } from "./styles";
 export const Introducao = () => {
   return (
-    <div>
-      A ascensão da inteligência artificial (IA) trouxe consigo uma série de
-      promessas e desafios. Enquanto as aplicações de IA têm o potencial de
-      transformar positivamente a sociedade, também suscitam questões éticas
-      profundas. Este artigo abordará três questões cruciais: racismo
-      algorítmico, privacidade de dados e o impacto social no mercado,
-      destacando exemplos, causas e estratégias para mitigar esses problemas
-      éticos.
-    </div>
+    <Fragment>
+      <AppBarHeader></AppBarHeader>
+      <Image></Image>
+      <div>
+        A ascensão da inteligência artificial (IA) trouxe consigo uma série de
+        promessas e desafios. Enquanto as aplicações de IA têm o potencial de
+        transformar positivamente a sociedade, também suscitam questões éticas
+        profundas. Este artigo abordará três questões cruciais: racismo
+        algorítmico, privacidade de dados e o impacto social no mercado,
+        destacando exemplos, causas e estratégias para mitigar esses problemas
+        éticos.
+      </div>
+    </Fragment>
   );
 };
---
+++ src/pages/Introducao/styles.ts
@@ -0,0 +1,6 @@
+import { styled } from "@mui/material";
+export const Image = styled("div")(() => ({
+  backgroundImage: 'url("src/assets/img/brainAI.png")',
+  height: "300px",
+  width: "300px",
+}));
