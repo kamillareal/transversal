3215f75f519473bae8d1831d66e30a3ab20184d5
Arthur Barata <arthurbs@motorola.com>
2023-09-25 19:55:55 -0400

    home nav

--- src/shared/components/app-bar/index.tsx
+++ src/shared/components/app-bar/index.tsx
@@ -3,9 +3,12 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import { useNavigate } from "react-router-dom";
+import { RoutesEnum } from "../../../routes/enum";
 import BasicMenu from "../menu";
 
 export default function AppBarHeader() {
+  const navigate = useNavigate();
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" sx={{ backgroundColor: "#007FFF" }}>
@@ -14,7 +17,14 @@ export default function AppBarHeader() {
             Ética relacionada à IA
           </Typography>
           <BasicMenu></BasicMenu>
-          <Button color="inherit">Home</Button>
+          <Button
+            color="inherit"
+            onClick={() => {
+              navigate(RoutesEnum.Home);
+            }}
+          >
+            Home
+          </Button>
         </Toolbar>
       </AppBar>
     </Box>
